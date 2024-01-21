50f66a7e69866dcdd9d15e69715c2168058e3979
Carlos Durana (HP) <carlos.durana@gmail.com>
2024-01-21 16:48:57 +0000

    Revert "Criar classe Conversoes e mover método"
    This reverts commit a73619417503180e6364ac10facc03c5d3558980.

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -7,7 +7,14 @@ using System.Threading.Tasks;
 namespace Calculos
 {
     internal static class Aritmetica
-    {        
+    {
+        public enum ConversaoTemperatura
+        {
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenheitCelsius
+        }
+        
         public static int Somar(int x, int y)
         {
             return x + y;
@@ -16,6 +23,20 @@ namespace Calculos
         public static int Subtrair(int x, int y)
         {
             return x - y;
+        }
+
+        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return (temperatura - 32) / 1.8;
+            }
+
+            return -1;
         }
     }
 }
